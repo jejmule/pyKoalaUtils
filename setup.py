d426806b098469813d24abc1750846a453360ab7
Jérôme Parent <jerome.parent@lynceetec.com>
2020-03-19 16:30:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Utility to open LynceeTec Koala bin files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/jejmule/pyKoalaUtils",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
