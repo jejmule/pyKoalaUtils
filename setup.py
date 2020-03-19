d64b5f71c17c5d3dbb503efabd2e0f0c3433ff21
Jérôme Parent <jerome.parent@lynceetec.com>
2020-03-19 18:32:25 +0100

    release 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyKoalaUtils", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Jerome Parent",
     author_email="jerome.parent@lyncetec.com",
     description="Utility to open LynceeTec Koala bin files",
